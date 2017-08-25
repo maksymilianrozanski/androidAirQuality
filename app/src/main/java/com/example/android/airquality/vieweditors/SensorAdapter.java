d49689d0ae08029bb58c132036ea1e5305c913bc
Max <maksymilian.rozanski@gmail.com>
2017-08-25 17:59:59 +0200

    add maximum acceptable values to sensor views

--- app/src/main/java/com/example/android/airquality/vieweditors/SensorAdapter.java
+++ app/src/main/java/com/example/android/airquality/vieweditors/SensorAdapter.java
@@ -12,13 +12,19 @@ import android.widget.TextView;
 import com.example.android.airquality.R;
 import com.example.android.airquality.dataholders.Sensor;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Max on 19.08.2017.
  */
 
 public class SensorAdapter extends ArrayAdapter<Sensor> {
+
+    //map holding maximum acceptable concentrations of parameters
+    private static final Map<String, Integer> MAX_CONCENTRATIONS;
 
     public SensorAdapter(@NonNull Context context, List<Sensor> sensors) {
         super(context, 0, sensors);
@@ -62,9 +68,16 @@ public class SensorAdapter extends ArrayAdapter<Sensor> {
         }
         String paramValueString = String.format("%.2f", paramValue);
 
+
+        //TODO: display the highest acceptable concentration
         //if sensorType is correctly loaded add units at the end of value string
         if (!sensorType.equals("not specified")) {
-            paramValueView.setText(paramValueString + "μg/m³");
+            String textToAdd = paramValueString;
+            //add maximum acceptable value
+            Integer maxValue = MAX_CONCENTRATIONS.get(sensorType);
+            textToAdd = textToAdd + "/" + maxValue + " μg/m³";
+            //add string to TextView
+            paramValueView.setText(textToAdd);
         }
 
         //find TextView that display date of measurement
@@ -79,5 +92,19 @@ public class SensorAdapter extends ArrayAdapter<Sensor> {
         }
         dateView.setText(date);
         return listItemView;
+    }
+
+    //set maximum acceptable values
+    //http://powietrze.gios.gov.pl/pjp/content/annual_assessment_air_acceptable_level
+    static{
+        Map<String, Integer> maxConcentrationsMap = new HashMap<String, Integer>();
+        maxConcentrationsMap.put("C6H6", 5);
+        maxConcentrationsMap.put("NO2", 200);
+        maxConcentrationsMap.put("SO2", 125);
+        maxConcentrationsMap.put("PM10", 50);
+        maxConcentrationsMap.put("CO", 10000);
+        maxConcentrationsMap.put("PM2.5", 25);
+        maxConcentrationsMap.put("O3", 120);
+        MAX_CONCENTRATIONS = Collections.unmodifiableMap(maxConcentrationsMap);
     }
 }
