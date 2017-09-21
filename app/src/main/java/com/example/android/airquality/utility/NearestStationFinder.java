4248b45f4b71216cb551354bc56f39cc86cbfb1c
Max <maksymilian.rozanski@gmail.com>
2017-09-21 21:13:29 +0200

    add printing correct nearest station

--- app/src/main/java/com/example/android/airquality/utility/NearestStationFinder.java
+++ app/src/main/java/com/example/android/airquality/utility/NearestStationFinder.java
@@ -15,13 +15,14 @@ public class NearestStationFinder {
      * @return id of station which is located the closest to user's location
      */
     public static Integer findNearestStation(double userLatitude, double userLongitude, List<Station> stations) {
-        double userDistance = 9999999;
+        double userDistance = Double.MAX_VALUE;
         Integer closestStationId = null;
         for (Station station : stations) {
             double stationLatitude = Double.parseDouble(station.getGegrLat());
             double stationLongitude = Double.parseDouble(station.getGegrLon());
             double currentDistance = calculateDistance(userLatitude, userLongitude, stationLatitude, stationLongitude);
-            if (currentDistance < userDistance){
+            if (currentDistance < userDistance) {
+                userDistance = currentDistance;
                 closestStationId = Integer.parseInt(station.getId());
             }
         }
@@ -30,26 +31,27 @@ public class NearestStationFinder {
 
     /**
      * Calculate distance between user and station using Haversine method, height is omitted
-     * @param userLatitude - user's position latitude
-     * @param userLongitude - user's position longitude
-     * @param stationLatitude - station's latitude
+     *
+     * @param userLatitude     - user's position latitude
+     * @param userLongitude    - user's position longitude
+     * @param stationLatitude  - station's latitude
      * @param stationLongitude - station's longitude
-     * @return  distance in meters
+     * @return distance in meters
      */
     private static double calculateDistance(double userLatitude, double userLongitude,
                                             double stationLatitude, double stationLongitude) {
-        final int R = 6371; // Radius of the earth
+        final int R = 6371000; // Radius of the earth
+        double userLatitudeRadian = Math.toRadians(userLatitude);
+        double userLongitudeRadian = Math.toRadians(userLongitude);
+        double stationLatitudeRadian = Math.toRadians(stationLatitude);
+        double stationLongitudeRadian = Math.toRadians(stationLongitude);
 
-        double latDistance = Math.toRadians(stationLatitude - userLatitude);
-        double lonDistance = Math.toRadians(stationLongitude - userLongitude);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(userLatitude)) * Math.cos(Math.toRadians(stationLatitude))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000; // convert to meters
+        double distance = (2 * R) * (Math.asin(Math.sqrt(
+                Math.pow(Math.sin((stationLatitudeRadian - userLatitudeRadian) / 2), 2)
+                        + Math.cos(userLatitudeRadian) * Math.cos(stationLatitudeRadian) *
+                        Math.pow(Math.sin((stationLongitudeRadian - userLongitudeRadian) / 2), 2)
+        )));
 
-        distance = Math.pow(distance, 2);
-
-        return Math.sqrt(distance); //distance in meters
+        return distance; //distance in meters
     }
 }
