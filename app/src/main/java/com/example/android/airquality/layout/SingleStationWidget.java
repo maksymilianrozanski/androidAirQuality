d1238fb8c287eca64088a8c37c8e64bfb8f9d81b
Max <maksymilianrozanski@gmail.com>
2017-10-19 20:16:27 +0200

    modify widget xml, modify default widget text

--- app/src/main/java/com/example/android/airquality/layout/SingleStationWidget.java
+++ app/src/main/java/com/example/android/airquality/layout/SingleStationWidget.java
@@ -6,7 +6,6 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.example.android.airquality.R;
@@ -23,7 +22,7 @@ public class SingleStationWidget extends AppWidgetProvider {
                                 int appWidgetId) {
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.single_station_widget);
-        views.setTextViewText(R.id.widgetStationName, "Text set from updateAppWidget");
+        views.setTextViewText(R.id.widgetStationName, "Tap to refresh");
 
         Intent intentSendToService = new Intent(context, WidgetUpdateService.class);
 
@@ -36,8 +35,6 @@ public class SingleStationWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.v("LOG", "Inside onUpdate....");
-
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
