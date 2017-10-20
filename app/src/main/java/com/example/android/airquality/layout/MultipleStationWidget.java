2b734117d461c89ae347d5d44eeabede288f06b7
Max <maksymilianrozanski@gmail.com>
2017-10-20 18:16:47 +0200

    add MultipleStationWidget class

---
+++ app/src/main/java/com/example/android/airquality/layout/MultipleStationWidget.java
@@ -0,0 +1,40 @@
+package com.example.android.airquality.layout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Max on 20.10.2017.
+ */
+
+public class MultipleStationWidget extends AppWidgetProvider {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        super.onReceive(context, intent);
+    }
+}
