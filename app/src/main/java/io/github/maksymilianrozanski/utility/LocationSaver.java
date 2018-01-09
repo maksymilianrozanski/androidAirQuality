7f6a3a8119eab617292b64e1b249dec98700006d
Max <maksymilianrozanski@gmail.com>
2018-01-09 18:44:08 +0100

    add LocationSaver class, remove empty line from build.gradle

---
+++ app/src/main/java/io/github/maksymilianrozanski/utility/LocationSaver.java
@@ -0,0 +1,39 @@
+package io.github.maksymilianrozanski.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+
+public class LocationSaver {
+
+    private Context context;
+    private static final String sharedPreferencesString = "io.github.maksymilianrozanski";
+    private static final String latitudeKey = "LatitudeKey";
+    private static final String longitudeKey = "LongitudeKey";
+    private static final String defaultLatitude = "0";
+    private static final String defaultLongitude = "0";
+
+
+    public LocationSaver(Context context) {
+        this.context = context;
+    }
+
+    public void saveLocation(Location location){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferencesString, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(latitudeKey, String.valueOf(location.getLatitude()));
+        editor.putString(longitudeKey, String.valueOf(location.getLongitude()));
+        editor.commit();
+    }
+
+    public Location getLocation(){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferencesString, Context.MODE_PRIVATE);
+        String latitude = sharedPreferences.getString(latitudeKey, defaultLatitude);
+        String longitude = sharedPreferences.getString(longitudeKey,defaultLongitude);
+
+        Location location = new Location("savedLocation");
+        location.setLatitude(Double.parseDouble(latitude));
+        location.setLongitude(Double.parseDouble(longitude));
+        return location;
+    }
+}
