3fa6b5140819205bfc4eb8368f6485d2a4730733
Max <maksymilianrozanski@gmail.com>
2018-12-14 12:48:22 +0100

    remove not used code

--- app/src/main/java/io/github/maksymilianrozanski/widget/MultipleStationWidgetContract.kt
+++ app/src/main/java/io/github/maksymilianrozanski/widget/MultipleStationWidgetContract.kt
@@ -2,21 +2,6 @@ package io.github.maksymilianrozanski.widget
 
 interface MultipleStationWidgetContract {
 
-    interface Presenter {
-        fun onDestroy()
-        fun refreshClicked()
-        fun stationClicked(stationId: Int, stationName: String)
-    }
-
-    interface MainView {
-        fun showProgress()
-        fun hideProgress()
-        fun displayError()
-        fun onResponseFailure(throwable: Throwable)
-        fun displayData(stations: List<WidgetItem>)
-        fun startSingleStationActivity(stationId: Int, stationName: String)
-    }
-
     interface Model {
         interface OnFinishedListener {
             fun onFinished(stations: List<WidgetItem>)
