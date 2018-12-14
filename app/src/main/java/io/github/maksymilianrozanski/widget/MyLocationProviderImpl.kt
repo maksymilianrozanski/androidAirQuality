45b9ce1ac9ffc00ba51e2c0385d5de8a4578736f
Max <maksymilianrozanski@gmail.com>
2018-12-14 13:48:40 +0100

    fix MyLocationProvider not providing location on SDK < 23

--- app/src/main/java/io/github/maksymilianrozanski/widget/MyLocationProviderImpl.kt
+++ app/src/main/java/io/github/maksymilianrozanski/widget/MyLocationProviderImpl.kt
@@ -3,27 +3,28 @@ package io.github.maksymilianrozanski.widget
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v4.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import io.github.maksymilianrozanski.utility.LocationSaver
 
 class MyLocationProviderImpl(var context: Context) : MyLocationProvider {
 
-    override fun getLocation(onFinishedListener: MyLocationProvider.OnFinishedListener, onFinishedToPass:MultipleStationWidgetContract.Model.OnFinishedListener) {
+    override fun getLocation(onFinishedListener: MyLocationProvider.OnFinishedListener, onFinishedToPass: MultipleStationWidgetContract.Model.OnFinishedListener) {
         val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-            fusedLocationProviderClient.lastLocation.addOnCompleteListener {result ->
+        if ((ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) || Build.VERSION.SDK_INT < 23) {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { result ->
                 val locationSaver = LocationSaver(context)
                 if (result.isSuccessful && result.result != null) {
                     locationSaver.saveLocation(result.result)
-                    onFinishedListener.onLocationReceived(result.result ?: locationSaver.location, onFinishedToPass)
-                }else {
+                    onFinishedListener.onLocationReceived(result.result
+                            ?: locationSaver.location, onFinishedToPass)
+                } else {
                     onFinishedListener.onLocationReceived(locationSaver.location, onFinishedToPass)
                 }
             }
         } else {
-            onFinishedListener.onLocationFailure(Throwable("Access to location not granted."), onFinishedToPass )
+            onFinishedListener.onLocationFailure(Throwable("Access to location not granted."), onFinishedToPass)
         }
     }
 }
